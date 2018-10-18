25c83212fc5ce72e5b2e23dd049fccd9e164fc58
brukk <bruk1892@gmail.com>
2018-10-18 16:00:29 -0600

    array helper exercises added to array4 dir

---
+++ array4/array4.js
@@ -0,0 +1,52 @@
+// Q1
+
+let inventorsBornInFifteen = [];
+inventors.filter(function(inventor){
+    if (inventor.year >= 1500 && inventor.year <= 1599){
+        inventorsBornInFifteen.push(inventor)
+    } 
+    return inventorsBornInFifteen;
+})
+console.table(inventorsBornInFifteen);
+
+//***************************************************** */
+//Q2
+
+ const inventorsFandL = inventors.map(function(inventor){
+    return `${inventor.first} ${inventor.last}`
+})
+console.log(inventorsFandL)
+
+//****************************************************** */
+//Q3
+
+const ordered = inventors.sort(function(person1, person2){
+    if (person1.year > person2.year) {
+        return 1;
+    }else {
+        return -1;
+    }
+})
+console.table(ordered)
+
+//******************************************************** */
+//Q4
+
+const totalYear = inventors.reduce(function(total,inventor){
+   return total + (inventor.passed - inventor.year) 
+}, 0)
+ console.log(totalYear)
+
+ //******************************************************** */
+ //Q5
+const livedOrder = inventors.sort(function(a,b){
+    if ((a.passed - a.year) > (b.passed - b.year)){
+        return 1;
+    }else {
+        return -1;
+    }
+})
+console.table(livedOrder)
+
+//******************************************************** */
+//Q6
